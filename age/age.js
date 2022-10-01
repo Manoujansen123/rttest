962b45e204e4e0b954bfc543728f207c8db2adb5
Manoujansen123 <94604958+Manoujansen123@users.noreply.github.com>
2022-10-01 14:13:24 +0200

    Add files via upload

---
+++ age/age.js
@@ -0,0 +1,117 @@
+
+
+function onClick21() {
+  var reset = window.location.reload();
+  document.getElementById("clicks21").innerHTML = reset;
+
+};
+
+
+var clicks = 0;
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+
+};
+
+var clicks2 = 0;
+
+function onClick2() {
+  clicks2 += 2;
+  document.getElementById("clicks2").innerHTML = clicks2;
+
+};
+
+var clicks3 = 0;
+
+function onClick3() {
+  clicks3 += 4;
+  document.getElementById("clicks3").innerHTML = clicks3;
+
+};
+
+var clicks4 = 0;
+
+function onClick4() {
+  clicks4 += 8;
+  document.getElementById("clicks4").innerHTML = clicks4;
+
+};
+
+var clicks5 = 0;
+
+function onClick5() {
+  clicks5 += 16;
+  document.getElementById("clicks5").innerHTML = clicks5;
+
+};
+
+var clicks6 = 0;
+
+function onClick6() {
+  clicks6 += 32;
+  document.getElementById("clicks6").innerHTML = clicks6;
+
+};
+
+var clicks7 = 0;
+
+function onClick7() {
+  clicks7 += 64;
+  document.getElementById("clicks7").innerHTML = clicks7;
+
+};
+
+var clicks8 = 0;
+
+function onClick8() {
+  clicks8 += 128;
+  document.getElementById("clicks8").innerHTML = clicks8;
+
+};
+
+var clicks9 = 0;
+
+function onClick9() {
+  clicks9 += 256;
+  document.getElementById("clicks9").innerHTML = clicks9;
+
+};
+
+var clicks10 = 0;
+
+function onClick10() {
+  clicks10 += 512;
+  document.getElementById("clicks10").innerHTML = clicks10;
+
+};
+
+var clicks11 = 0;
+
+function onClick11() {
+  clicks11 += 1024;
+  document.getElementById("clicks11").innerHTML = clicks11;
+
+};
+
+var clicks12 = 0;
+
+function onClick12() {
+  clicks12 += 2048;
+  document.getElementById("clicks12").innerHTML = clicks12;
+
+};
+
+var clicks13 = 0;
+
+function onClick13() {
+  clicks13 += 4096;
+  document.getElementById("clicks13").innerHTML = clicks13;
+
+};
+
+function onClick20() {
+    document.getElementById("clicks20").innerHTML = click13, click12, clicks11, clicks10, clicks9, clicks8, clicks7, clicks6, clicks5, clicks4, clicks3, clicks2, clicks;
+
+  };
